39ff71b03c17ccb9a044e7c50e6127be73c69267
sdidd <77382851+sdidd@users.noreply.github.com>
2022-01-01 10:35:28 +0530

    Update graphmaker.py

--- cogs/graphmaker.py
+++ cogs/graphmaker.py
@@ -7,4 +7,15 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 from mcstatus import MinecraftServer
+# line_plot.py
 
+import matplotlib.pyplot as plt
+
+def line_plot(numbers):
+    plt.plot(numbers)
+    plt.ylabel('Random numbers')
+    plt.show()
+
+if __name__ == '__main__':
+    numbers = [2, 4, 1, 6]
+    line_plot(numbers)
