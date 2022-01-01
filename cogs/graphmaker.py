33a30b4a1e59d83ebeeadd0745801ed52e23ffa3
sdidd <77382851+sdidd@users.noreply.github.com>
2022-01-01 10:18:20 +0530

    Update graphmaker.py

--- cogs/graphmaker.py
+++ cogs/graphmaker.py
@@ -1,1 +1,10 @@
 import discord
+import random
+import os
+import asyncio
+from discord.ext import tasks
+from discord.ext import commands
+from discord.ext.commands import Bot
+from dotenv import load_dotenv
+from mcstatus import MinecraftServer
+
